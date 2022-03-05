42e1adce66a016a9d21d90c05202b9fe9be9bf2d
Krygin Andrey <readysteel@ya.ru>
2022-03-05 16:02:43 +0200

    krygin 3

--- krygin.java
+++ krygin.java
@@ -4,6 +4,6 @@ import java.util.EventObject;
 
 public class RobotActionEvent extends EventObject {
     public RobotActionEvent(Object source) { 
-        super(source); 
+        super(source); // 3
     } 
 } 
