39dd5225fc739571089a344707c6e15e139f30e5
Gumerov Marat <max_person@mail.ru>
2022-03-05 17:04:21 +0300

    marat 3

--- gumerov.java
+++ gumerov.java
@@ -18,7 +18,7 @@ public class Temperature extends AbstractCharacteristic
 
     @Override
     public Color drawColor() {
-        return new Color(200,200,200,70);
+        return new Color(0,0,0,70);
     }
 
     @Override
