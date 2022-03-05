dec4e5f1cf0388ea6bd5861b09a94b7fefb38de2
Shehu <asusoft@gmail.com>
2022-03-05 15:57:33 +0300

    Shehu first commit on main

--- shehu.java
+++ shehu.java
@@ -14,7 +14,7 @@ public class GameField {
 	
     public GameField ()
     {
-        setSize(10, 10);
+        setSize(9, 10);
     }
         
     public final void setSize(int width, int height)
