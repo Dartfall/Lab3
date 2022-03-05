d700f248fb83484a8849d354703e6424441a50a5
AndreySidor <andrey.sidor.01@mail.ru>
2022-03-05 15:45:58 +0300

    Andrey: 1

--- sidor.java
+++ sidor.java
@@ -1,7 +1,7 @@
 package brokenrobotgame.model;
 
 import brokenrobotgame.model.navigation.CellPosition;
-
+//gt
 abstract public class AbstractBattery
 {
     private boolean _isDestroy = false;     
